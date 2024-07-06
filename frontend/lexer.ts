9f087f8d04b2f56a3902d3bd777be3466ab6fb91
Connor Park <connorpark024@gmail.com>
2024-07-06 10:27:05 -0400

    finish additive and multiplicative expr

--- frontend/lexer.ts
+++ frontend/lexer.ts
@@ -51,7 +51,8 @@ export function tokenize(sourceCode: string): Token[] {
       src[0] == "+" ||
       src[0] == "-" ||
       src[0] == "*" ||
-      src[0] == "/"
+      src[0] == "/" ||
+      src[0] == "%"
     ) {
       tokens.push(token(src.shift(), TokenType.BinaryOperator));
     } else if (src[0] == "=") {
@@ -87,10 +88,4 @@ export function tokenize(sourceCode: string): Token[] {
 
   tokens.push({ type: TokenType.EOF, value: "EndOfFile" });
   return tokens;
-}
-
-const source = await Deno.readTextFileSync("test.txt");
-
-for (const token of tokenize(source)) {
-  console.log(token);
 }
