6d1e96a35f21513fbad89585416991c4807f090e
Connor Park <connorpark024@gmail.com>
2024-07-06 09:29:18 -0400

    initial commit

---
+++ ast.ts
@@ -0,0 +1,36 @@
+export type Nodetype =
+  | "Program"
+  | "NumericLiteral"
+  | "Identifier"
+  | "BinaryExpr"
+  | "CallExpr"
+  | "UnaryExpr"
+  | "FunctionDeclaration";
+
+export interface Stmt {
+  kind: Nodetype;
+}
+
+export interface Program extends Stmt {
+  kind: "Program";
+  body: Stmt[];
+}
+
+export interface Expr extends Stmt {}
+
+export interface BinaryExpr extends Expr {
+  kind: "BinaryExpr";
+  left: Expr;
+  right: Expr;
+  operator: string;
+}
+
+export interface Identifier extends Expr {
+  kind: "Identifier";
+  symbol: string;
+}
+
+export interface NumericLiteral extends Expr {
+  kind: "NumericLiteral";
+  value: number;
+}
